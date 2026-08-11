5979dad8ad390505c426147e0882dffc12d926d6
hayleycoe989 <hc989@exeter.ac.uk>
2026-08-11 09:51:23 +0100

    Update ExeRainforestPopup.svelte

--- src/lib/components/ExeRainforestPopup.svelte
+++ src/lib/components/ExeRainforestPopup.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
-  const { props } = $props<{ props: any }>();
+  const p = $props();
 </script>
 
-<div class="p-3 text-sm">
-  <h3 class="font-bold">Exe Rainforest</h3>
-  <pre class="text-xs">{JSON.stringify(props, null, 2)}</pre>
+<div class="p-3 text-sm space-y-3">
+  <h3 class="font-bold">Possible Site of Temperate Rainforest</h3>
+  <pre class="text-xs">{JSON.stringify(p, null, 2)}</pre>
+
+  <div class="pt-2 border-t">
+    <a
+      href="https://map.lostrainforestsofbritain.org/#"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="text-blue-600 underline text-xs"
+    >
+      View on Lost Rainforests of Britain map
+    </a>
+  </div>
 </div>
